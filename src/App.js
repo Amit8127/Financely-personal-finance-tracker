101165e8d13b8a5dfdb9b5e8cb47bc3058bc9b03
Amit Kumar <amitmonu0000@gmail.com>
2024-01-09 03:08:55 +0530

    remove import header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Header from './components/Header/Header';
 import SignUp from './pages/Signup';
 import Dashboard from './pages/Dashboard';
 
